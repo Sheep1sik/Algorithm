d2190012b889e889afb6773c030a1b65379c3902
Sheep1sik <bohg6799@naver.com>
2024-01-10 22:01:02 +0900

    Refactor: Error검출 코드 추가

--- LeetCode/LeetCode_1929.playground/Contents.swift
+++ LeetCode/LeetCode_1929.playground/Contents.swift
@@ -1,8 +1,43 @@
 import UIKit
 
+// 정답 코드
+//class Solution {
+//    func getConcatenation(_ nums: [Int]) -> [Int] {
+//        var array = nums
+//        return array + array
+//    }
+//}
+
+
+// 리펙토링 코드
+enum error: Error {
+    case invalidLength
+    case invalidElement
+}
+
 class Solution {
-    func getConcatenation(_ nums: [Int]) -> [Int] {
-        var array = nums
+    
+    func getConcatenation(_ nums: [Int]) throws -> [Int] {
+        let n = nums.count
+        
+        guard n >= 1 && n <= 1000 else {
+            throw error.invalidLength
+        }
+        guard nums.allSatisfy({$0 >= 1 && $0 <= 1000}) else {
+            throw error.invalidElement
+        }
+        
+        let array = nums
         return array + array
     }
+}
+
+var test = Solution()
+var nums = [1,3,2,1]
+
+do {
+    let result = try test.getConcatenation(nums)
+    print(result)
+} catch {
+    print(error)
 }
