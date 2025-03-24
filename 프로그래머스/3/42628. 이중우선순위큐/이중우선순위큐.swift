6666d7de098bfaa5b176be2b00b9b3a3e43d9f9e
_GGDol <bohg6799@naver.com>
2025-03-24 09:18:52 +0900

    [level 3] Title: 이중우선순위큐, Time: 2905.49 ms, Memory: 21.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
@@ -1,29 +1,5 @@
 import Foundation
 
 func solution(_ operations:[String]) -> [Int] {
-    var queue = [Int]()
-    
-    for op in operations {
-        let parts = op.split(separator: " ")
-        let command = parts[0]
-        let value = Int(parts[1])!
-        
-        if command == "I" {
-            queue.append(value)
-            queue.sort()
-        } else if command == "D" {
-            if queue.isEmpty { continue }
-            if value == 1 {
-                queue.removeLast()
-            } else if value == -1 {
-                queue.removeFirst()
-            }
-        }
-    }
-    
-    if queue.isEmpty {
-        return [0, 0]
-    } else {
-        return [queue.last!, queue.first!]
-    }
+    return []
 }
