bfbf9cc9ef6a0a7c406b7a873973a63d121e3f4e
_GGDol <bohg6799@naver.com>
2025-04-01 10:11:17 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -4,15 +4,12 @@ func solution(_ brown: Int, _ yellow: Int) -> [Int] {
     let total = brown + yellow
 
     for height in 3...total {
-        if total % height == 0 {
-            let width = total / height
-            if width >= height {
-                if (width - 2) * (height - 2) == yellow {
-                    return [width, height]
-                }
+        let width = total / height
+        if width >= height {
+            if (width - 2) * (height - 2) == yellow {
+                return [width, height]
             }
         }
     }
-    
     return []
 }
